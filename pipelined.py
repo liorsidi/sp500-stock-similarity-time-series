487a014048ced57035f3ec3efa6a2997bacb36f9
LIOR-E7270\Lior <liorsid@post.bgu.ac.il>
2018-01-13 12:19:04 +0200

    processing pipeline

--- pipelined.py
+++ pipelined.py
@@ -93,23 +93,23 @@ class model_bases_distance(object):
     def __init__(self, model):
         self.model = model
 
-    def stock_prep(self,stock_X):
+    def stock_norm_prep(self,stock_X):
         numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
         numeric_cols = stock_X.select_dtypes(include=numerics).columns.tolist()
         numeric_cols_prep = [s + "_prep" for s in numeric_cols]
 
         mms = MinMaxScaler()
 
-        train_stock_X_names_df = stock_X[[ENTITY, TARGET]]
+        train_stock_X_names_df = stock_X[[ENTITY, TARGET]].reset_index(drop=True)
         stock_X_norm = mms.fit_transform(stock_X[numeric_cols])
-        train_stock_X_norm_df = pd.DataFrame(stock_X_norm, columns=numeric_cols_prep)
+        train_stock_X_norm_df = pd.DataFrame(stock_X_norm, columns=numeric_cols_prep).reset_index(drop=True)
         train_stock_X_prep = pd.merge(train_stock_X_names_df, train_stock_X_norm_df, left_index=True, right_index=True)
 
         return train_stock_X_prep
 
     def fit(self, stock_X):
-        stock_X_prep = self.stock_prep(stock_X)
-        x, y, p = prepare_stock_windows(stock_X_prep, 10,1, [1], [TARGET])
+        stock_X_prep = self.stock_norm_prep(stock_X)
+        x, y, p = prepare_stock_windows(stock_X_prep, 10,1, [1], [TARGET+ '_prep'])
         self.model.fit(x,y)
 
     def apply_distance(self, stock_X):
@@ -119,8 +119,8 @@ class model_bases_distance(object):
         :param stock2:
         :return:
         """
-        stock_X_prep = self.stock_prep(stock_X)
-        x, y, p = prepare_stock_windows(stock_X_prep, 10, 1, [1], [TARGET])
+        stock_X_prep = self.stock_norm_prep(stock_X)
+        x, y, p = prepare_stock_windows(stock_X_prep, 10, 1, [1], [TARGET + '_prep'])
         preds = self.model.predict(x)
 
         return mean_squared_error(y,preds)
@@ -152,7 +152,7 @@ def get_similarity(df_stocks, stock_to_compare, stock_names, similarity_func, ex
             stock_X = df_stocks[df_stocks[ENTITY] == stock_to_compare]
             similarity_func.fit(stock_X)
             similarities = [
-                similarity_func.apply_distance(df_stocks[df_stocks[ENTITY] == stock_name][TARGET])
+                similarity_func.apply_distance(df_stocks[df_stocks[ENTITY] == stock_name])
                 for stock_name in stock_names
                 ]
         else:
@@ -318,7 +318,6 @@ def prepare_rolling_periods(df_stocks, stock_to_compare,
     for stock_name in top_stocks.keys():
         train_stock_X = train_X[train_X[ENTITY] == stock_name].reset_index(drop = True)
         train_stock_X_names_df = train_stock_X[[ENTITY, TARGET]]
-        #del train_stock_X[ENTITY]
         train_stock_X_norm =  preprocessing_pipeline.fit_transform(train_stock_X[numeric_cols])
         train_stock_X_norm_df = pd.DataFrame(train_stock_X_norm, columns=numeric_cols_prep)
         train_stock_X_prep = pd.merge(train_stock_X_names_df, train_stock_X_norm_df, left_index=True, right_index=True)
@@ -333,7 +332,6 @@ def prepare_rolling_periods(df_stocks, stock_to_compare,
 
             test_stock_X = test_X[test_X[ENTITY] == stock_name].reset_index(drop=True)
             test_stock_X_names_df = test_stock_X[[ENTITY, TARGET]]
-            # del test_stock_X[ENTITY]
             test_stock_X_norm = preprocessing_pipeline.fit_transform(test_stock_X[numeric_cols])
             test_stock_X_norm_df = pd.DataFrame(test_stock_X_norm, columns=numeric_cols_prep)
             test_stock_X_prep = pd.merge(test_stock_X_names_df, test_stock_X_norm_df, left_index=True, right_index=True)
@@ -392,7 +390,9 @@ def prepare_folds(df_stocks, stock_to_compare, window_len, slide,next_t, n_folds
     folds_Y_test = []
     folds_price_test = []
     folds_topk = []
+    # for each fold
     for f in range(n_folds-1):
+        # define rolling times by the tagets period
         start_period_train = stock_times[f*period_len]
         end_period_train = stock_times[f*period_len + period_len]
         start_period_test = stock_times[f*period_len + period_len]
@@ -520,18 +520,21 @@ def evaluate_model(window_len, folds_X_train, folds_Y_train, folds_X_test, folds
     future_ys = folds_Y_train[0].columns.tolist()
     evaluations = []
 
+    #iterate folds
     for f in range(len(folds_X_train)):
         features = [(fe + '_prep', wl) for fe in features_selection[1] for wl in range(window_len)]
         X_train = folds_X_train[f][features]
         X_test = folds_X_test[f][features]
         #X_test_curr_price = X_test[(TARGET , window_len - 1)].tolist()
 
+        # iterate future value to predict
         for t in future_ys:
             y_train = folds_Y_train[f][t].values
             y_test = folds_Y_test[f][t].values
 
             model = model_class(**model_args)
             lb = LabelBinarizer()
+            # if classifier then descrtisize target value
             if not isinstance(model, RegressorMixin):
                 X_train_curr_price = X_train[(TARGET + "_prep", window_len - 1)].tolist()
                 y_train = target_discretization(X_train_curr_price, y_train)
@@ -544,6 +547,7 @@ def evaluate_model(window_len, folds_X_train, folds_Y_train, folds_X_test, folds
             model.fit(X_train, y_train)
             y_preds = model.predict(X_test)
 
+            # if regressor then predict if will go up or down by the diffrence
             if isinstance(model, RegressorMixin):
                 y_preds = y_preds - y_test
 
@@ -557,6 +561,7 @@ def evaluate_model(window_len, folds_X_train, folds_Y_train, folds_X_test, folds
                 eval_error["value"] = evaluation_method(y_test,y_preds)
                 evaluations.append(eval_error)
 
+            # if classifier then evaluate with relevant measures
             if not isinstance(model, RegressorMixin):
                 for evaluation_method in [roc_auc_score]:
                     y_proba = model.predict_proba(X_test)
@@ -603,6 +608,42 @@ def get_index_product(params):
         params_product_dicts.append(params_dict)
 
     return params_product_dicts
+
+
+def save_evaluations(data_period, evaluations, experiment_path, features_selection, folds_topk, k, n_folds,
+                     preprocessing_pipeline, select_k_func, similarity_func, slide, stock_to_compare,
+                     weighted_sampleing, window_len):
+    model_eval = pd.concat(evaluations)
+    model_eval['data_period'] = data_period
+    model_eval['stock_to_compare'] = stock_to_compare
+    model_eval['preprocessing_pipeline'] = " ".join(list(preprocessing_pipeline.named_steps.keys()))
+    model_eval['weighted_sampleing'] = weighted_sampleing
+    model_eval['similarity_func'] = similarity_func.__name__
+    model_eval['k'] = k
+    model_eval['select_k_func'] = select_k_func.__name__
+    model_eval['window_len'] = window_len
+    model_eval['slide'] = slide
+    model_eval['features_selection'] = features_selection[0]
+    similar_stock_eval_folds = []
+    for f in range(n_folds - 1):
+        similar_stock_eval_fold = {}
+        similar_stock_eval_fold['fold'] = f
+        similar_stock_eval_fold['data_period'] = data_period
+        similar_stock_eval_fold['stock_to_compare'] = stock_to_compare
+        similar_stock_eval_fold['preprocessing_pipeline'] = preprocessing_pipeline.named_steps.keys
+        similar_stock_eval_fold['similarity_func'] = similarity_func.__name__
+        similar_stock_eval_fold['k'] = k
+        similar_stock_eval_fold['select_k_func'] = select_k_func.__name__
+
+        for name, distance in folds_topk[f].items():
+            stock_f = dict(similar_stock_eval_fold)
+            stock_f['name'] = name
+            stock_f['distance'] = distance
+            similar_stock_eval_folds.append(stock_f)
+    model_eval_path = os.path.join(experiment_path, 'models_evaluations.csv')
+    sim_eval_path = os.path.join(experiment_path, 'similarity_evaluations.csv')
+    model_eval.to_csv(model_eval_path, mode='a')
+    pd.DataFrame(similar_stock_eval_folds).to_csv(sim_eval_path, mode='a')
 
 
 def run_experiment(data_period, stock_to_compare, preprocessing_pipeline,features_selection,
@@ -654,39 +695,11 @@ def run_experiment(data_period, stock_to_compare, preprocessing_pipeline,feature
                                   [mean_squared_error], [simple_profit_evaluation, long_short_profit_evaluation],
                                   target_discretization) for model in models]
 
-    model_eval = pd.concat(evaluations)
-    model_eval['data_period'] = data_period
-    model_eval['stock_to_compare'] = stock_to_compare
-    model_eval['preprocessing_pipeline'] = " ".join(list(preprocessing_pipeline.named_steps.keys()))
-    model_eval['weighted_sampleing'] = weighted_sampleing
-    model_eval['similarity_func'] = similarity_func.__name__
-    model_eval['k'] = k
-    model_eval['select_k_func'] = select_k_func.__name__
-    model_eval['window_len'] = window_len
-    model_eval['slide'] = slide
-    model_eval['features_selection'] = features_selection[0]
+    save_evaluations(data_period, evaluations, experiment_path, features_selection, folds_topk, k, n_folds,
+                     preprocessing_pipeline, select_k_func, similarity_func, slide, stock_to_compare,
+                     weighted_sampleing, window_len)
 
-    similar_stock_eval_folds = []
-    for f in range(n_folds-1):
-        similar_stock_eval_fold = {}
-        similar_stock_eval_fold['fold'] = f
-        similar_stock_eval_fold['data_period'] = data_period
-        similar_stock_eval_fold['stock_to_compare'] = stock_to_compare
-        similar_stock_eval_fold['preprocessing_pipeline'] = preprocessing_pipeline.named_steps.keys
-        similar_stock_eval_fold['similarity_func'] = similarity_func.__name__
-        similar_stock_eval_fold['k'] = k
-        similar_stock_eval_fold['select_k_func'] = select_k_func.__name__
 
-        for name, distance in folds_topk[f].items():
-            stock_f = dict(similar_stock_eval_fold)
-            stock_f['name'] = name
-            stock_f['distance'] = distance
-            similar_stock_eval_folds.append(stock_f)
-
-    model_eval_path = os.path.join(experiment_path, 'models_evaluations.csv')
-    sim_eval_path = os.path.join(experiment_path, 'similarity_evaluations.csv')
-    model_eval.to_csv(model_eval_path,mode = 'a')
-    pd.DataFrame(similar_stock_eval_folds).to_csv(sim_eval_path, mode='a')
 
 
 def main():
@@ -697,12 +710,12 @@ def main():
         'n_folds' : [6],
         # tech, finance, service, health, consumer, Industrial
         'stock_to_compare' : ["GOOGL"], #, "JPM", "DIS", "JNJ", "MMM", "KO", "GE"],
-        'k' : [1,  10, 50],
-        'select_k_func' : [get_random_k, get_top_k ],#, get_top_k],
+        'k' : [10, 1, 50],
+        'select_k_func' : [get_top_k, get_random_k],#, get_top_k],
         'window_len' : [10],#, 5, 20],
         'slide' : [1],#, 3, 5, 10],
         'preprocessing_pipeline' : [Pipeline([('minmax_normalize', MinMaxScaler())])],
-        'similarity_func' : [model_bases_distance(RandomForestClassifier(100)),apply_euclidean, apply_pearson,apply_dtw],
+        'similarity_func' : [model_bases_distance(RandomForestRegressor(100,random_state = 0)),apply_euclidean, apply_pearson,apply_dtw],
         'weighted_sampleing': [True, False],
         'target_discretization' : [statistical_targeting],
         'features_selection': [#('full_features' ,[u'Open',u'High',u'Low',u'Close',u'Volume']),
@@ -714,9 +727,9 @@ def main():
         {
             'next_t': [1, 3, 7],
             'models': [RandomForestClassifier, RandomForestRegressor,GradientBoostingRegressor],
-            'models_arg' : {RandomForestClassifier.__name__: {'n_estimators': 100},
-                            RandomForestRegressor.__name__: {'n_estimators': 100},
-                            GradientBoostingRegressor.__name__: {'learning_rate': 0.02}}
+            'models_arg' : {RandomForestClassifier.__name__: {'n_estimators': 100, 'random_state' : 0},
+                            RandomForestRegressor.__name__: {'n_estimators': 100, 'random_state' : 0},
+                            GradientBoostingRegressor.__name__: {'learning_rate': 0.02,'random_state' : 0}}
         }
 
     experiments = get_index_product(experiment_params)
